3be982b41a03d60216c528a124240d42f73bc5f7
scott <scottsaunders@mac.com>
2023-12-04 09:25:50 -0500

    Update index.php
    Added sample code for template specific styles.

--- index.php
+++ index.php
@@ -16,6 +16,18 @@
     }
 }
 add_action( 'wp_enqueue_scripts', 'my_enqueue_styles' );
+
+/* For a Specific Template - Reduces chances of style collisions 
+function my_enqueue_styles() {
+     if ( ! is_admin() ) {
+         // Check if the current page is using a specific template
+         if ( is_page_template( 'full-width.php' ) ) {
+             wp_enqueue_style( 'custom-styles', plugin_dir_url( __FILE__ ) . 'custom-style.css', array(), '1.1', 'all' );
+         }
+     }
+ }
+ add_action( 'wp_enqueue_scripts', 'my_enqueue_styles' );
+*/ 
   
   // Adds preview stuff for the backend.
   function preview_stuff()
